83b55a56e499521353c0de03266153b9668626c0
tomas <toomiileon@gmail.com>
2024-11-19 11:33:43 -0300

    agregue la lista de contactos

--- js/app.js
+++ js/app.js
@@ -1,26 +1,46 @@
 import Contacto from "./classContacto.js";
 
 //declaro variables globales
-const nuevoContacto = new Contacto(
-  "juan",
-  "perez",
-  "juanperez@mail.com",
-  "2281567890",
-  "juancho",
-  "tucuman",
-  "https://campus.rollingcodeschool.com/course/view.php?id=230"
+
+const modalContacto = new bootstrap.Modal(
+  document.getElementById("contactModal")
 );
-
-const modalContacto = new bootstrap.Modal(document.getElementById('contactModal'))
-const btnAgregar = document.querySelector('.btn-primary')
-
+const btnAgregar = document.querySelector(".btn-primary");
+const formulario = document.getElementById("contactForm");
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+const apodo = document.querySelector("#apodo");
+const email = document.querySelector("#email");
+const telefono = document.querySelector("#telefono");
+const direccion = document.querySelector("#direccion");
+const imagen = document.querySelector("#urlImagen");
+const listaContactos = []
 
 //funciones
 
-function abrirModalContacto(){
-    modalContacto.show()
+function abrirModalContacto() {
+  modalContacto.show();
+}
+
+function crearContacto(e) {
+  e.preventDefault();
+  //validar los datos del formulario
+  //crear el objeto
+  const nuevoContacto = new Contacto(
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value,
+    apodo.value,
+    direccion.value,
+    imagen.value
+  );
+  //guardar el contacto en la lista
+  listaContactos.push(nuevoContacto)
+  console.log(nuevoContacto)
 }
 
 //el resto de la logica del proyecto
 
-btnAgregar.addEventListener('click', abrirModalContacto)
+btnAgregar.addEventListener("click", abrirModalContacto);
+formulario.addEventListener("submit", crearContacto);
