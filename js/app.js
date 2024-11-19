219f1e8e17bcfc4ff535938f7cd721f26438b23e
tomas <toomiileon@gmail.com>
2024-11-19 10:34:39 -0300

    importamos la clase contacto y creamos un objeto de prueba

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import Contacto from "./classContacto.js";
+
+const nuevoContacto = new Contacto(
+  "juan",
+  "perez",
+  "juanperez@mail.com",
+  "2281567890",
+  "juancho",
+  "tucuman",
+  "https://campus.rollingcodeschool.com/course/view.php?id=230"
+);
+
+console.log(nuevoContacto)
